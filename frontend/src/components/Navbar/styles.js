6a429b1e47b20a46c76f88c5f0242bd696a47e8b
rupali100 <32296575+rupali100@users.noreply.github.com>
2023-07-11 02:49:59 +0530

    Update styles.js- code refinement

--- frontend/src/components/Navbar/styles.js
+++ frontend/src/components/Navbar/styles.js
@@ -24,20 +24,8 @@ export default makeStyles((theme) => ({
       boxShadow: "none",
     },
   },
-  cartt: {
-    "&:hover": {
-      color: "#ffff",
-      boxShadow: "none",
-    },
-  },
   image: {
     marginRight: "10px",
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-    [theme.breakpoints.up("sm")]: {
-      display: "none",
-    },
   },
 
   grow: {
@@ -45,18 +33,6 @@ export default makeStyles((theme) => ({
     "&:hover": {
       color: "#000000",
       fontSize: "120%",
-    },
-  },
-  inputRoot: {
-    color: "inherit",
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 0),
-    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("md")]: {
-      width: "20ch",
     },
   },
 }));
